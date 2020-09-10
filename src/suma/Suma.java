3ab79b5855e69838e5960b0e1cf9c611cf153974
Gaston <Hyouka21@github.com>
2020-09-10 13:47:46 -0300

    Initial commit

---
+++ src/suma/Suma.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma;
+
+import interfaz.suma;
+
+
+
+/**
+ *
+ * @author Alumno
+ */
+public class Suma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       suma s=new suma();// constructor
+       s.setVisible(true); //hacer visible la pantalla
+    }
+    
+}
